9bafc54ab220aab7fa1b97f6320d30d82b933a8c
Lukáš Tížek <lukas.tizek@firma.seznam.cz>
2018-08-14 06:53:17 +0200

    add nameday

--- mirror/DateTime.py
+++ mirror/DateTime.py
@@ -3,6 +3,8 @@
 
 import pyglet
 import time
+import requests
+import json
 
 from Helper import *
 
@@ -11,15 +13,23 @@ class DateTime:
 		self.config = config
 		self.batch = pyglet.graphics.Batch()
 
-		self.time = Helper.label(self, '', 90, -10, 120, self.batch, window, config['DATETIME'])
+		self.time = Helper.label(self, '', 90, -10, 125, self.batch, window, config['DATETIME'])
 		self.date = Helper.label(self, '', 50, 0, 0, self.batch, window, config['DATETIME'])
+		self.nameDay = Helper.label(self, '', 15, 5, -80, self.batch, window, config['DATETIME'])
 
 		self.tick()
+		self.getNameDay()
 
 
 	def tick(self, *args):
 		self.time.text = time.strftime(self.config['DATETIME']['timeformat'])
 		self.date.text = time.strftime(self.config['DATETIME']['dateformat'])
+
+
+	def getNameDay(self):
+		nameDay = requests.get('https://api.abalin.net/get/today?country=cz');
+		name = json.loads(nameDay.text)
+		self.nameDay.text = 'dnes má svátek {:s}'.format(name['data']['name_cz'])
 
 
 	def draw(self):
